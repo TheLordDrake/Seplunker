ac198e639504741a2d93c11070c58cf1ca4e5ecc
Iain F <iain.forsgren@silvertech.com>
2019-03-07 13:29:44 -0500

    Added render engine
    Changed to windows app to hide default console. Added font file.

---
+++ Spelunker/Engine.cs
@@ -0,0 +1,50 @@
+﻿using RLNET;
+
+namespace Spelunker
+{
+    internal class Engine
+    {
+        private readonly RLRootConsole _rootConsole;
+
+        private int _playerX;
+        private int _playerY;
+
+        public Engine(RLRootConsole console)
+        {
+            _rootConsole = console;
+            _rootConsole.Render += Render;
+            _rootConsole.Update += Update;
+        }
+
+        private void Render(object sender, UpdateEventArgs e)
+        {
+            _rootConsole.Clear();
+            _rootConsole.Set(_playerX, _playerY, RLColor.White, null, '@');
+            _rootConsole.Draw();
+        }
+
+        private void Update(object sender, UpdateEventArgs e)
+        {
+            RLKeyPress keyPress = _rootConsole.Keyboard.GetKeyPress();
+
+            if (keyPress != null)
+            {
+                switch (keyPress.Key)
+                {
+                    case RLKey.Up:
+                        _playerY--;
+                        break;
+                    case RLKey.Down:
+                        _playerY++;
+                        break;
+                    case RLKey.Left:
+                        _playerX--;
+                        break;
+                    case RLKey.Right:
+                        _playerX++;
+                        break;
+                }
+            }
+        }
+    }
+}
--- Spelunker/Program.cs
+++ Spelunker/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RLNET;
 
 namespace Spelunker
 {
     class Program
     {
-        static void Main(string[] args)
+        private static RLRootConsole _rootConsole;
+
+        public static void Main(string[] args)
         {
+            // Path to Font img, width of window in tiles, height of window in tiles, width of tile in pixels, height of tile in pixels
+            _rootConsole = new RLRootConsole("assets/terminal8x8.png", 60, 40, 8, 8);
+            var engine = new Engine(_rootConsole);
+            _rootConsole.Run();
         }
     }
 }
